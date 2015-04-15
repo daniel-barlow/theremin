1c6c405d8ef6076e8e7df39ae0654495118a757c
Daniel Barlow <daniel.barlow@simplybusiness.co.uk>
2015-04-15 16:43:35 +0100

    wip

--- src/theremin/core.clj
+++ src/theremin/core.clj
@@ -1,43 +1,51 @@
 (ns theremin.core
-  (:require [overtone.inst.drum :as drum])
+  (:require [overtone.inst.drum :as drum]
+            [clojure.core.async :as async :refer [>! <! >!! <!!
+                                                  chan go go-loop alts!!]])
   (:use [overtone.live]))
 
 
-(definst foo [] (saw 220))
 
 (definst trem [freq 440 depth 10 rate 6 length 3]
   (* 0.3
-     (line:kr 0 1 length o/FREE)
+     (line:kr 0 1 length FREE)
      (saw (+ freq (* depth (sin-osc:kr rate))))))
 
-(definst bass [freq 110 vol 0.5 length 0.2]
-  (* (/ vol 2)
+(definst bass [freq 110 length 0.2 vol 0.5]
+  (* vol
      (line:kr 1 0 length FREE)
      (+ (sin freq) (* 0.4 (+ (saw freq) (saw (/ freq 2)))))))
 
 
-(defn play-tune [click start-time fun notes]
-  (dorun
-   (map
-    (fn [[n note [ & dur]]]
-      (at (click (+ start-time n))
-          (fun note 0.9 (or dur 0.3))))
-    notes)))
 
-(defn incessantly [click n fun]
-  (at (click n) (fun))
-  (let [n1 (inc n)] (apply-by (click n1) #'incessantly click n1 fun [])))
+;; requirements:
+
+;; 1) controller gestures should affect the music in a timely fashion,
+;; when it's about volume, filters, etc
+;; 2) there is a general pattern of 16 bar blocks.  Changes that affect the
+;; melody or overall composition happen at block boundaries
 
 
-(let [metro (metronome 140)
-      metro2 (metronome 280)
-      play (partial play-tune metro2)
-      bassline [[0 100]
-                [1 100 0.4]
-                [2 100]
-                [3 200]]]
-  (dorun (incessantly metro2 0 #(drum/closed-hat 0.2)))
-  (dorun (incessantly metro 0 #(drum/dub-kick 60)))
-  (dorun (play 0 bass bassline)))
 
+
+(defn compose-block []
+  (concat
+   (map #(vector (* % 4) 'drum/dub-kick 60) (range 0 16))
+   (map #(vector (* % 2) 'bass (+ 110 (* 65 (mod % 4)))) (range 0 32))))
+
+
+(defn play-block [click block]
+  (map (fn [[tick inst & params]]
+         (prn inst)
+         (at (click tick) (apply (eval inst) params)))
+       block))
+
+(play-block (metronome (* 4 140)) (compose-block))
+
+
+
+
+
+
+#_
 (stop)
